c99c2ae538e6b14bcd3c3eed92b239599052a534
Vinay Homkar <homkar2015@gmail.com>
2024-08-10 00:51:25 +0530

    success form

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,19 +5,36 @@ import { parseStringify } from "../utils";
 
 export const createAppointment = async (
     appointment: CreateAppointmentParams
-  ) => {
+) => {
     try {
-      const newAppointment = await databases.createDocument(
-        DATABASE_ID!,
-        APPOINTMENT_COLLECTION_ID!,
-        ID.unique(),
-        appointment
-        
-      );
-  
-      //revalidatePath("/admin");
-      return parseStringify(newAppointment);
+        const newAppointment = await databases.createDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            ID.unique(),
+            appointment
+
+        );
+
+        //revalidatePath("/admin");
+        return parseStringify(newAppointment);
     } catch (error) {
-      console.error( error);
+        console.error(error);
     }
-  };
+};
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId
+        );
+
+        return parseStringify(appointment);
+    } catch (error) {
+        console.error(
+            "An error occurred while retrieving the existing patient:",
+            error
+        );
+    }
+};
